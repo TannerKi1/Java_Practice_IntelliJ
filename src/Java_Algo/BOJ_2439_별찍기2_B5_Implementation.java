cf0c3252acb137a7278809e0a890199268d971b2
Tanner <monokase18@gmail.com>
2022-08-16 14:56:26 +0900

    New Problem

---
+++ src/Java_Algo/BOJ_2439_별찍기2_B5_Implementation.java
@@ -0,0 +1,29 @@
+package Java_Algo;
+
+import java.io.*;
+
+
+public class BOJ_2439_별찍기2_B5_Implementation {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        br.close();
+
+        for (int i = 1; i <= a; i++){
+
+            for (int k = 1; k <= a - i; k++){
+                bw.write(" ");
+            }
+
+            for (int j = 1 ; j <= i; j++){
+                bw.write("*");
+
+            }
+            bw.write("\n");
+        }
+        bw.close();
+
+    }
+}
