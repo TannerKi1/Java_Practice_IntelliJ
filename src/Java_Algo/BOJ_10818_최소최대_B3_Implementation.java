7e1c6305ea1a8edbeabb33aac25fd6893c948b4c
Tanner <monokase18@gmail.com>
2022-08-16 22:41:09 +0900

    New Problem

---
+++ src/Java_Algo/BOJ_10818_최소최대_B3_Implementation.java
@@ -0,0 +1,27 @@
+package Java_Algo;
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class BOJ_10818_최소최대_B3_Implementation {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int a = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int min = 1000001;
+        int max = -1000001;
+
+        while (st.hasMoreTokens()){
+            int val = Integer.parseInt(st.nextToken());
+            if (val > max){
+                max = val;
+            }
+            if (val < min){
+                min = val;
+            }
+        }
+        System.out.println(min + " " + max);
+        }
+}
